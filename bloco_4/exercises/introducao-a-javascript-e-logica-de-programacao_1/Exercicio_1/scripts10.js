70cd122d0b2555aea9daef30c6150dd034913100
denis-rossati <denis.rossati@hotmail.com>
2021-02-24 19:23:16 -0300

    exercicio 11 resolvido

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/Exercicio_1/scripts10.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/Exercicio_1/scripts10.js
@@ -4,4 +4,8 @@ let valorImposto = ((custo*80)/100);
 let numVendas= 1000;
 let thousand = (numVendas*valorImposto);
 
-console.log (thousand);
+if (valor <= 0 || custo <= 0 || valorImposto <= 0 || numVendas <= 0 || thousand <= 0) {
+    console.log ("erro, uma de suas entradas é menor que zero.");
+} else {
+    console.log (thousand);
+}
