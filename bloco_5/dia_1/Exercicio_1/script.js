e89f0bc548bf63d3744892a64711556310086b4d
denis-rossati <denis.rossati@hotmail.com>
2021-03-03 15:56:43 -0300

    esqueci de dar os commits... requisito 1, 2 e 3 finalizados

--- bloco_5/dia_1/Exercicio_1/script.js
+++ bloco_5/dia_1/Exercicio_1/script.js
@@ -1,9 +1,15 @@
 //acessa o <p> do documento e o modifica
 document.getElementsByTagName("p")[1].innerText = "Formado na trybe e num bom emprego";
 
+function changeColor(color){
+  document.getElementsByClassName("main-content").style.backgroundColor = "green";
+}
 
+function corretText() {
+document.getElementsByTagName("h2").innerText = "Exercício 5.1 - JavaScript";
+}
 
-
+corretText();
 
 
 
