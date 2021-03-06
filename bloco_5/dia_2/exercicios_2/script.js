8526cdd9453043ff25704ee5d02519fad5543d5d
denis-rossati <denis.rossati@hotmail.com>
2021-03-06 14:58:50 -0300

    corrigido requisito 1

--- bloco_5/dia_2/exercicios_2/script.js
+++ bloco_5/dia_2/exercicios_2/script.js
@@ -18,18 +18,27 @@ createDaysOfTheWeek();
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 function addDaysOfMonth() {
   for (let x = 0; x < dezDaysList.length; x += 1) {
+    let counterHoliday = (x + 10);
     let docLi = document.createElement("li");
     let docUl = document.getElementById("days");
     docUl.appendChild(docLi);
     docLi.innerText = dezDaysList[x]
     docLi.className = "day"
+
+    if ((x == 6) || (x == 13) || (x == 20)) {
+      document.querySelectorAll("li")[((8 + x))].className = "day friday";
+    }    
+
+    if ((x == 26) || (x == 33)) {
+      document.querySelectorAll("li")[((8 + x))].className = "day holiday";
+    } 
+
+    if ((x == 27)) {
+      document.querySelectorAll("li")[((8 + x))].className = "day friday holiday";
+    }
   }
-  document.getElementsByTagName("li")[25].className = "day holiday"
-  document.getElementsByTagName("li")[26].className = "day holiday"
-  document.getElementsByTagName("li")[30].className = "day holiday"
-  document.getElementsByTagName("li")[14].className = "day friday"
-  document.getElementsByTagName("li")[21].className = "day friday"
-  document.getElementsByTagName("li")[28].className = "day friday"
-  document.getElementsByTagName("li")[35].className = "day friday"
 }
 addDaysOfMonth();
+
+
+
