d385446d05e84dd9c4c3ae464bc406b80183ce63
denis-rossati <denis.rossati@hotmail.com>
2021-03-23 21:54:27 -0300

    exercicio finalizado

--- script.js
+++ script.js
@@ -11,9 +11,8 @@
   }
   return result;
 }
-fatorialNumber(5); */
 
-/* const biggestWord = string => {
+const biggestWord = string => {
   let arrayWords = string.split(' ');
   let sizeWord = arrayWords[0];;
   for (let x = 0; x < arrayWords.length; x += 1) {
@@ -24,23 +23,36 @@ fatorialNumber(5); */
     console.log(sizeWord);
   }
 }
-biggestWord("Antônio foi no banheiro e não sabemos o que aconteceu") */
-/* 
-const botao = document.getElementById('botao');
-const contador = document.getElementById('contador');
-localStorage.clicadas == undefined ? localStorage.clicadas = 0 : console.log('de boa');
-contador.innerText = (localStorage.clicadas);
-let clickCounter = Number(localStorage.clicadas);
-botao.addEventListener('click', () => {
-  contador.innerText = clickCounter += 1
-  localStorage.clicadas = Number(contador.innerText);
-}); */
+const activeButton = () => {
+  const botao = document.getElementById('botao');
+  const contador = document.getElementById('contador');
+  localStorage.clicadas == undefined ? localStorage.clicadas = 0 : console.log('de boa');
+  contador.innerText = (localStorage.clicadas);
+  let clickCounter = Number(localStorage.clicadas);
+  botao.addEventListener('click', () => {
+    contador.innerText = clickCounter += 1
+    localStorage.clicadas = Number(contador.innerText);
+  });
+} 
+biggestWord("Antônio foi no banheiro e não sabemos o que aconteceu");
+fatorialNumber(5);
+activeButton();
+*/
+
+let stringSkills = ['HTML;' , 'CSS;' , 'JavaScript;' , 'Paginas Responsivas;' , 'Bootstrap;'];
 
 const stringFunction = string => {
-  let stringX = "Finalmente, achamos o X enterrado!"
-  let stringArray = stringX.split('X')
+  let stringX = "Tryber x aqui!"
+  let stringArray = stringX.split('x')
   let stringComplete = `${stringArray[0]}${string}${stringArray[1]}`
   return stringComplete;
 }
-
-stringFunction('tesouro')
+const stringLiteral = string => {
+  let stringSort = stringSkills.sort();
+  for(let x = 0; x < stringSkills.length; x += 1) {
+    stringSkills[x] += '\n';
+  }
+  let novaString = ` ${string}\n Minhas cinco principais habilidades são:\n ${stringSort.join(' ')} #GoTrybe`
+  return novaString;
+}
+stringLiteral(stringFunction('Denis'));
