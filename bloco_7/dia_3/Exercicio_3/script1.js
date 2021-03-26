b8146e464d9a5b4b225eab559d1ae76fb68deb29
denis-rossati <denis.rossati@hotmail.com>
2021-03-26 03:08:20 -0300

    requisito 1 finalizdao

---
+++ bloco_7/dia_3/Exercicio_3/script1.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const greetPeople = (people) => {
+  let greeting = [];
+
+  for (const person in people) {
+    greeting.push(`Hello ${people[person]}`);
+  }
+  return greeting;
+};
+
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+assert.deepStrictEqual(typeof greetPeople, 'function');
+assert.deepStrictEqual(greetPeople(parameter), result);
