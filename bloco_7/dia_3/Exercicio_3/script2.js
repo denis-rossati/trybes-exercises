a52fec9549fdf42983b7595b1410fe9caf8a6ddf
denis-rossati <denis.rossati@hotmail.com>
2021-03-26 03:18:33 -0300

    requisito 2 finalizado

--- bloco_7/dia_3/Exercicio_3/script2.js
+++ bloco_7/dia_3/Exercicio_3/script2.js
@@ -1,7 +1,9 @@
+const assert = require('assert');
+
 const removeVowels = (word) => {
   const characters = word.split('');
-  const results = [];
-
+  let results = '';
+  let numbers = 1;
   for (let index = 0; index < characters.length; index += 1) {
     if (
       characters[index] === 'a' ||
@@ -10,14 +12,16 @@ const removeVowels = (word) => {
       characters[index] === 'e' ||
       characters[index] === 'u'
     ) {
-      results.push(characters[index]);
+      results += (numbers);
+      numbers += 1;
     } else {
-      results.push('_');
+      results += (characters[index]);
     }
   }
   return results;
 };
 
-
 const parameter = 'Dayane';
 const result = 'D1y2n3';
+
+assert.deepStrictEqual(removeVowels(parameter), result);
