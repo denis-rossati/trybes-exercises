efe1e7f2e62e00584cae7e327290048952b63cc8
denis-rossati <denis.rossati@hotmail.com>
2021-03-30 18:53:04 -0300

    requsito 2 finalizado

---
+++ bloco_8/dia_2/Pratica_1/script.js
@@ -0,0 +1,15 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Use o método forEach chamando a callback showEmailList para apresentar os emails.
+emailListInData.forEach((email) => {
+  showEmailList(email);
+});
