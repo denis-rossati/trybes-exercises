8dfd13596c7b73dced1c8e1a137db040ac84eab9
denis-rossati <denis.rossati@hotmail.com>
2021-04-07 00:38:48 -0300

    exercicio 1 feito

--- bloco_8/dia_4/Exercicio_1/exercise1.js
+++ bloco_8/dia_4/Exercicio_1/exercise1.js
@@ -7,23 +7,8 @@ const arrays = [
     [4, 5, 6],
 ];
 
-/* function flatten() {
-  // escreva seu código aqui
-  let array1 = []
-  for (let x = 0; x < arrays.length; x += 1) {
-    for ( let y = 0; y < arrays[x].length; y += 1) {
-      array1.push(arrays[x][y])
-    }
-  }
-  return array1
-} */
-
-
 function flatten() {
   // escreva seu código aqui
-  arrays.map((element) => {
-    
-  })
+  return arrays.reduce((arrayVazio, array) => arrayVazio.concat(array.filter((elemento) => elemento)), [])
 }
-
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
