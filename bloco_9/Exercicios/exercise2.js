f877089e393dfd38b21e383e8ad11c5297bcda77
denis-rossati <denis.rossati@hotmail.com>
2021-04-12 18:17:41 -0300

    requisito 2 finalizado

---
+++ bloco_9/Exercicios/exercise2.js
@@ -0,0 +1,35 @@
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+// 'A' não depende de espera alguma, será o primeiro a aparecer
+console.log(planetDistanceFromSun(mars)); // A
+
+// O tempo de espera do B é maior que o de C, então vai aparecer após ele.
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+// A, C e B.
