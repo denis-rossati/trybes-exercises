ba3b2dc04c3c4ab7d61b7cfa5d2890cd1cef3343
denis-rossati <denis.rossati@hotmail.com>
2021-04-19 20:04:00 -0300

    commit provisorio

--- bloco_10/dia_1/exercicio1/testes/compare.test.js
+++ bloco_10/dia_1/exercicio1/testes/compare.test.js
@@ -1,14 +1,8 @@
 const { TestScheduler } = require('@jest/core');
 const { obj1, obj2, obj3 } = require('../compare');
 
-
-
 describe('Testando o arquivo compare.js', () => {
   const errorMessage = 'Um dos objetos não é igual ao outro';
-  const object = {
-    title: 'My Title',
-    description: 'My Description',
-  };
   // o toEqual aparentemente compara os valores. Seu eu fizesse
   //   test(errorMessage, () => expect(obj1).not.toEqual(obj2));
   // o teste falharia, pois apesar das chaves conterem valores organizados de maneira diferente, eles são iguais...
