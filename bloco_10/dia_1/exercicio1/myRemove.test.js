e6a363757d9efd5994871a5be22fe650e929b3d0
denis-rossati <denis.rossati@hotmail.com>
2021-04-19 19:11:28 -0300

    exercicio 3 finalizado

--- bloco_10/dia_1/exercicio1/myRemove.test.js
+++ bloco_10/dia_1/exercicio1/myRemove.test.js
@@ -1,11 +1,5 @@
 const myRemove = require('./myRemove');
 
-/* Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-Verifique se o array passado por parâmetro não sofreu alterações
-Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
- */
-
 describe('Testando a função myRemove', () => {
   const errorMessageNotExpected = 'O array passado por parametro não retorna o valor esperado'
   // O toBe não reconhece arrays/funções/objetos/tipos não primitivos 'iguais' como iguais.
