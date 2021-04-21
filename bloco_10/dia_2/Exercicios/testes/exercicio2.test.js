5bc3f4da3526eeb7fecdf52654b6da029305bc73
denis-rossati <denis.rossati@hotmail.com>
2021-04-21 17:02:12 -0300

    FINALMENTE, parte 3 finalizada

--- bloco_10/dia_2/Exercicios/testes/exercicio2.test.js
+++ bloco_10/dia_2/Exercicios/testes/exercicio2.test.js
@@ -11,3 +11,20 @@ describe('Testing getUserName function', () => {
     return getUserName(6).catch(error => expect(error).toBe(error));
   })
 });
+
+
+// tentando com async/await e try/catch na parte dos erros
+describe('Testing getUserName function', () => {
+  it('should return "Mark"', async () => {
+    const informacao = await getUserName(4);
+    return expect(informacao).toBe('Mark');
+  })
+  
+  it('should return a error message', async () => {
+    try {
+      await getUserName(6);
+    } catch(e) {
+      expect(e).toStrictEqual({"error": "User with 6 not found."});
+    }
+  })
+});
