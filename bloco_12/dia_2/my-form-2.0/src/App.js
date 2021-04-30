214368e4f599676b58055ab1b83a5eb371d6fd3f
denis-rossati <denis.rossati@hotmail.com>
2021-04-30 20:05:11 -0300

    parando por hoje

--- bloco_12/dia_2/my-form-2.0/src/App.js
+++ bloco_12/dia_2/my-form-2.0/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import './App.css';
 import GenericInput from './componentes/GenericInput';
+import NameInput from './componentes/NameInput';
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.handleGenericInput = this.handleGenericInput.bind(this);
+    this.chewUpperCaseGenericInput = this.chewUpperCaseGenericInput.bind(this);
+    this.chewSpecialCharactersGenericInput = this.chewSpecialCharactersGenericInput.bind(this);
+    this.chewFirstLetterGenericInput = this.chewFirstLetterGenericInput.bind(this);
     this.state = {
       name:'',
+      email:'',
+      cpf:'',
+      endereco:'',
+      cidade:'',
+      estado:'',
+      tipo:'',
     }
   }
 
@@ -17,14 +27,47 @@ class App extends React.Component {
     this.setState(() => ({
       [name]: value,       
     }));
-    console.log(target.name)
   }
 
+  checkFirstCharacter({ target }) {
+    if(!isNaN(target.value[0])) {
+      target.value = '';
+    }
+  }
+
+  toUpperCase({ target }) {
+    target.value = target.value.toUpperCase();
+  }
+
+  removeSpecialCharacters({ target }) {
+    target.value = target.value.replace(/[&\/\\#,+();^´`$~%.'":*!?<>{}]/g, '');
+  }
+
+  chewSpecialCharactersGenericInput(event) {
+    this.removeSpecialCharacters(event);
+    this.handleGenericInput(event);
+  }
+
+  chewUpperCaseGenericInput(event) {
+    this.toUpperCase(event);
+    this.handleGenericInput(event);
+  }
+
+  chewFirstLetterGenericInput(event) {
+    this.checkFirstCharacter(event);
+    this.handleGenericInput(event)
+  }
   render() {
     return (
-      <fieldset>
-        <GenericInput name='name' title='Nome:' maxLength='40' type='text' func={this.handleGenericInput}/>
-      </fieldset>
+      <form>
+        <fieldset>
+          <NameInput name='name' title='Nome:' maxLength='40' type='text' func={this.chewUpperCaseGenericInput} />
+          <GenericInput name='email' title='E-mail:' maxLength='50' type='text' func={this.handleGenericInput} />
+          <GenericInput name='cpf' title='CPF:' maxLength='11' type='text' func={this.handleGenericInput} />
+          <GenericInput name='endereco' title='Endereço:' maxLength='200' type='text' func= { this.chewSpecialCharactersGenericInput } />
+          <GenericInput name='cidade' title='Cidade:' maxLength='28' type='text' func={ this.chewFirstLetterGenericInput } />
+        </fieldset>
+      </form>
     );
   }
 }
